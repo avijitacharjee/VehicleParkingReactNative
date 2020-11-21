316470679d137cbc14923b29ec95e94918323d72
avijitach@gmail.com <avijitach@gmail.com>
2020-11-21 19:43:02 +0600

    nav page added

---
+++ src/Components/index/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native-animatable';
+import {
+    Text
+} from 'react-native';
+class Nav extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Index page
+                </Text>
+            </View>
+        )
+    }
+}
+export default Nav;
