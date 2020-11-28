48d54a35bfb29a2771cd472e852d8afe79b3af74
AvijitAcharjee <avijitach@gmail.com>
2020-11-28 17:52:52 +0600

    dashboard styles

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import { StyleSheet , Dimensions } from 'react-native';
 import {
     View,
     Text
 } from 'react-native-animatable'
+const {width,height} = Dimensions.get('window');
+export default class Dashboard extends React.Component {
+    state = {
+
+    };
+    render() {
+        return (
+            <>
+                <div className={styles.body}>
+
+                </div>    
+            </>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection : "row"
+    },
+    body: {
+        width: width*.8,
+        height: height * .8,
+        backgroundColor: '#dddddd',
+    }
+});
