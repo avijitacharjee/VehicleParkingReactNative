19ed13ed56999a56999f88bd90c6eef71d62b2cc
AvijitAcharjee <avijitach@gmail.com>
2020-11-29 02:18:35 +0600

    app registry problems

--- android/app/src/main/java/com/vehicleparking/MainApplication.java
+++ android/app/src/main/java/com/vehicleparking/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import in.sriraman.sharedpreferences.RNSharedPreferencesReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
